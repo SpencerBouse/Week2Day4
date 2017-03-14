4cba599808d1814e0d3e144a5cba25246051ab3e
Spencer Bouse <Orangesrb@gmail.com>
2017-03-14 02:38:37 -0400

    mobile menu

--- main.js
+++ main.js
@@ -1,58 +1,86 @@
-var buttonBlue = document.getElementById('blueButton');
-var buttonPink = document.getElementById('pinkButton');
-var buttonOrange = document.getElementById('orangeButton');
+var buttonone = document.getElementById('oneButton');
+var buttontwo = document.getElementById('twoButton');
+var buttonthree = document.getElementById('threeButton');
 var beforeTaxes = document.getElementById('beforeTax');
 var afterTaxes = document.getElementById('afterTax');
-var bluePressed = false;
-var pinkPressed = false;
-var orangePressed = false;
+var cart = document.getElementById('cart');
+var menu = document.getElementById('bars');
+var onePressed = false;
+var twoPressed = false;
+var threePressed = false;
+var menustatus = false;
 var subtotal = 0;
-
+var idnumber = 0;
+var cartArray = [];
+var d = new Date();
+var shirt1 = {
+  name:"Redboi",
+  price:29,
+  id:0
+}
+var shirt2 = {
+  name:"Symphony of the Night",
+  price:19
+}
+var shirt3 = {
+  name:"Iron Pixel",
+  price:39
+}
+function cartdiv(){
+  string = '';
+  cartArray.forEach(function(item){
+    string+=`
+    <div class="cartitem">
+      <p>${item.name}</p>
+      <p>$${item.price}</p>
+      <p id=${item.id}> </p>
+    </div>
+    `;
+  })
+}
 function taxes(calc){
-  return((calc * 1.0625).toFixed(2));
+  return((calc * 1.065).toFixed(2));
 }
 
-buttonBlue.addEventListener('click',function(){
-  if(bluePressed===false){
-      bluePressed = true;
-      subtotal += 29;
-      beforeTaxes.innerHTML = subtotal;
-      afterTaxes.innerHTML = taxes(subtotal);
-  }
-  else{
-    bluePressed = false;
-    subtotal -= 29;
-    beforeTaxes.innerHTML = subtotal;
-    afterTaxes.innerHTML = taxes(subtotal);
+menu.addEventListener('click',function(){
+  if(menustatus === false){
+    document.getElementById('jsmenu').style.display = 'block';
+    menustatus = true;
+  }else{
+    document.getElementById('jsmenu').style.display = 'none';
+    menustatus = false;
   }
 })
 
-buttonPink.addEventListener('click',function(){
-  if(pinkPressed===false){
-      pinkPressed = true;
-      subtotal += 19;
-      beforeTaxes.innerHTML = subtotal;
-      afterTaxes.innerHTML = taxes(subtotal);
-  }
-  else{
-    pinkPressed = false;
-    subtotal -= 19;
-    beforeTaxes.innerHTML = subtotal;
-    afterTaxes.innerHTML = taxes(subtotal);
-  }
+buttonone.addEventListener('click',function(){
+  cartArray.push(shirt1);
+  cartdiv();
+  cart.innerHTML = string;
+  idnumber ++;
+  shirt1.id = idnumber;
+  beforeTaxes.innerHTML = (subtotal += shirt1.price);
+  afterTaxes.innerHTML = taxes(subtotal);
+  console.log(shirt1.id);
 })
 
-buttonOrange.addEventListener('click',function(){
-  if(orangePressed===false){
-      orangePressed = true;
-      subtotal += 38;
-      beforeTaxes.innerHTML = subtotal;
-      afterTaxes.innerHTML = taxes(subtotal);
-  }
-  else{
-    orangePressed = false;
-    subtotal -= 38;
-    beforeTaxes.innerHTML = subtotal;
-    afterTaxes.innerHTML = taxes(subtotal);
-  }
+buttontwo.addEventListener('click',function(){
+  cartArray.push(shirt2);
+  cartdiv();
+  cart.innerHTML = string;
+  idnumber ++;
+  shirt2.id = idnumber;
+  beforeTaxes.innerHTML = (subtotal += shirt2.price);
+  afterTaxes.innerHTML = taxes(subtotal);
+  console.log(shirt2.id);
+})
+
+buttonthree.addEventListener('click',function(){
+  cartArray.push(shirt3);
+  cartdiv();
+  cart.innerHTML = string;
+  idnumber ++;
+  shirt3.id = idnumber;
+  beforeTaxes.innerHTML = (subtotal += shirt3.price);
+  afterTaxes.innerHTML = taxes(subtotal);
+  console.log(shirt3.id);
 })
